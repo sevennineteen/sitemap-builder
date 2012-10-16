46418ecc9f5cf5bcb0a61ef18473cec3c6d11bcb
Patric DelCioppo <patric.delcioppo@acquitygroup.com>
2012-10-16 09:49:11 -0400

    added comments

--- sitemap_builder.py
+++ sitemap_builder.py
@@ -4,6 +4,7 @@ import gzip
 from xml.sax.saxutils import escape
 
 class Sitemap(file):
+    "Represents a Sitemap file in XML format."
     
     # Sitemap protocol constraints
     xml_schema = {'xmlns': 'http://www.sitemaps.org/schemas/sitemap/0.9'}
@@ -16,21 +17,26 @@ class Sitemap(file):
     reporting_interval = 10000
 
     def __init__(self, path, compress=False):
+        "Instantiates the sitemap at supplied path, with optional gzip compression."
         self.path = path
         self.compress = compress
 
     def open(self, mode='r', encoding='utf-8'):
+        "Opens the sitemap file."
         self.file = codecs.open(self.path, mode, encoding)
         logging.debug('Opened file: %s' % (self.path))
 
     def close(self):
+        "Closes the sitemap file."
         self.file.close()
         logging.debug('Closed file: %s' % self.path)
 
     def writeline(self, content):
+        "Writes a line into the sitemap file."
         self.file.write(content + '\n')
 
     def make_gzip(self):
+        "Creates a GZIP-compressed version of the sitemap file."
         self.gzip_path = self.path + '.gz'
         f_in = open(self.path, 'rb')
         f_out = gzip.open(self.gzip_path, 'wb')
@@ -40,6 +46,7 @@ class Sitemap(file):
         logging.info('Gzipped file as: %s' % self.gzip_path)
 
     def begin(self):
+        "Begins writing the sitemap file, including XML declaration and root element."
         self.open('w')
         self.writeline(self.xml_declaration)
         self.writeline('<%s %s>' % (self.root_elem, ' '.join(
@@ -49,6 +56,7 @@ class Sitemap(file):
         logging.info('Started writing file: %s' % self.path)
 
     def add_entry(self, **kwargs):
+        "Writes the provided entry into the sitemap file."
         self.writeline('  <%s>' % self.entry_elem)
 
         for p in self.properties:
@@ -64,6 +72,7 @@ class Sitemap(file):
             logging.info('Processed %s entries; last entry: %s' % (self.entries, str(kwargs)))
 
     def end(self):
+        "Finishes writing the sitemap file, and initiates GZIP-compression if appropriate."
         self.writeline('</%s>' % self.root_elem)
         self.close()
         logging.info('Finished writing file: %s [%s entries]' % (self.path, self.entries))
@@ -72,12 +81,14 @@ class Sitemap(file):
             self.make_gzip()
 
     def _get_closed(self):
+        "Returns True if sitemap is closed."
         return not self.file or self.file.closed
 
     closed = property(_get_closed)
 
 
 class SitemapIndex(Sitemap):
+    "Represents a Sitemap Index file in XML format."
 
     # Sitemap protocol constraints
     root_elem = 'sitemapindex'
